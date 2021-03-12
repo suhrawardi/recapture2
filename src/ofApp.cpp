9fd2b8e2b0b3bbfd2ed3730a5149e83a5aaac665
jarra <suhrawardi@gmail.com>
2021-03-12 12:16:38 +0100

    A working version

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,23 +1,115 @@
 #include "ofApp.h"
 
-//--------------------------------------------------------------
-void ofApp::setup(){
+ofFbo fbo;
+ofShader shader;
 
+ofVideoPlayer video1;
+// ofVideoPlayer video2;
+ofVideoGrabber camera;
+ofSoundStream micInput;
+
+float soundLevel;
+float elapsedTime;
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+    shader.load("fragShader");
+
+    fbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);
+    
+    ofSetWindowTitle("Video thingies");
+    ofSetWindowShape(1280, 780);
+    ofSetFrameRate(30);
+    ofBackground(ofColor::white);
+    
+    video1.load("twee.mov");
+//    video2.load("twee.mov");
+    video1.play();
+//    video2.play();
+    
+    camera.setDeviceID(0);
+    camera.setDesiredFrameRate(30);
+    camera.initGrabber(1280, 720);
+    
+    soundLevel = 0.0;
+    
+    ofSoundStreamSettings settings;
+    auto devices = micInput.getMatchingDevices("default");
+    if (!devices.empty()) {
+        settings.setInDevice(devices[0]);
+    }
+    settings.setInListener(this);
+    settings.sampleRate = 44100;
+    settings.numOutputChannels = 0;
+    settings.numInputChannels = 1;
+    settings.bufferSize = 44100;
+    micInput.setup(settings);
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
+    video1.update();
+    // video2.update();
+    if (camera.isInitialized()) camera.update();
+    
+    elapsedTime = ofGetElapsedTimef();
 
+    // std::printf("Elapsed %f\n", elapsedTime);
+    // std::printf("Noise %f\n", ofNoise(elapsedTime));
+    // std::printf("RMS %f\n", video1Alpha);
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
+    if (camera.isInitialized()) {
+        shader.begin();
+        fbo.begin();
+        ofSetBackgroundAuto(false);
+        ofEnableAlphaBlending();
+        ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+        ofDisableSmoothing();
+        
+        shader.setUniformTexture("texture0", camera.getTexture(), 0);
+        shader.setUniformTexture("texture1", video1.getTexture(), 1);
+        
+        float alphaLevel = soundLevel * 50.0;
+        if (alphaLevel > 0.8) alphaLevel = 0.8;
+        if (alphaLevel < 0.4) alphaLevel = 0.4;
+        
+        // std::printf("sound %f\n", alphaLevel);
+        shader.setUniform1f("time", elapsedTime);
+        shader.setUniform1f("alpahVal", soundLevel);
+            
+        video1.draw(0, 0, ofGetWidth(), ofGetHeight());
+        //ofEnableAlphaBlending();
+        
+        camera.draw(0, 0, ofGetWidth(), ofGetHeight());
+        //ofEnableAlphaBlending();
+            
+        ofEnableSmoothing();
 
+        fbo.end();
+        shader.end();
+            
+        fbo.draw(0, 0, ofGetWidth(), ofGetHeight());
+    }
+}
+
+void ofApp::audioIn(ofSoundBuffer & buffer) {
+    // std::printf("RMS %f\n", buffer.getRMSAmplitude());
+    soundLevel = buffer.getRMSAmplitude();
+}
+
+void ofApp::exit() {
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::keyPressed(int key) {
+    if ( key == ' ' ) {
+     ofImage image;
+     image.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+     image.save("screen.png");
+    }
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -8,6 +8,8 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
+        void exit();
+        void audioIn(ofSoundBuffer & buffer);
 
 		void keyPressed(int key);
 		void keyReleased(int key);
